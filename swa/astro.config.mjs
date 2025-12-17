c493a9913951467b0fa1d7239379e81022933079
Cedric Ahlers <55838528+clowa@users.noreply.github.com>
2025-12-17 17:29:54 +0000

    add support to render markdown alerts with github style

--- swa/astro.config.mjs
+++ swa/astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
+import { remarkAlert } from "remark-github-blockquote-alert";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    markdown: {
+    remarkPlugins: [remarkAlert],
+  },
+});
