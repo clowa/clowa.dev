bc52e28776a92ea0e9384b748a772b0d21459dc7
Cedric Ahlers <55838528+clowa@users.noreply.github.com>
2025-12-30 09:56:27 +0000

    feat: add Rybbit tracking script to page

--- swa/src/env.d.ts
+++ swa/src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly PUBLIC_RYBBIT_SITE_ID: number
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
