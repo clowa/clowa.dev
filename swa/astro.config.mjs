3b87ef5ccf3770f6baf40178ba7d7bd3eff403e1
Cedric Ahlers <55838528+clowa@users.noreply.github.com>
2025-12-31 12:37:26 +0000

    feat: add sitemap to increase search engine visibility

--- swa/astro.config.mjs
+++ swa/astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
 import { remarkAlert } from "remark-github-blockquote-alert";
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-    markdown: {
+  site: 'https://clowa.dev',
+  markdown: {
     remarkPlugins: [remarkAlert],
   },
-  site: 'https://clowa.dev',
+  integrations: [sitemap({
+    customPages: [],
+  })],
 });
